804dec9265ddd05a62aa5a4a5dcbb47a40a68f0d
PhatNguyen1309 <phatnguyen13092003@gmail.com>
2024-07-10 09:51:07 +0700

    add AnhDebug va program

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2001210345_NguyenTanPhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            string[] names = { "Nguyen", "Tan", "Phat"};
+
+            foreach (string name in names)
+            {
+                int length = name.Length;
+                total += length;
+
+                Console.WriteLine("Tên: " + name + ", Độ dài: " + length);
+
+            }
+
+            Console.WriteLine("Tổng: " + total);
+
+            Console.ReadLine();
+        }
+    }
+}
